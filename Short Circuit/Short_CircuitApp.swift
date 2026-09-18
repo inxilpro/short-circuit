5fb8d4837067c3d62726f67772aa25be37b01756
Chris Morrell <inxilpro@users.noreply.github.com>
2026-09-18 13:15:32 -0400

    Initial Commit

---
+++ Short Circuit/Short_CircuitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Short_CircuitApp.swift
+//  Short Circuit
+//
+//  Created by Chris Morrell on 9/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Short_CircuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
